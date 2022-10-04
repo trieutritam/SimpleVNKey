32548be04c37bb963fe5e1695e1e0373f446d38c
Trieu Tri Tam <trieutritam@gmail.com>
2022-10-04 15:17:36 +0700

    Add some test cases

--- kbengine/src/test/fixture/kbengine-test.cc
+++ kbengine/src/test/fixture/kbengine-test.cc
@@ -9,11 +9,16 @@ using namespace std;
 #include <kbengine/platforms/macos.h>
 #include <kbengine/vnlexi.h>
 
+#include "keycode-map.h"
+
 // this is unicode mask used by engine
 #define UNICODE_MASK 0x00010000
 
 class KbEngineTest: public ::testing::Test {
   protected:
+    kbengine engine;
+    map<UInt32, vector<UInt16>> charList;
+
     void SetUp() override {
       charList = codeTable[0];  // Unicode encoding
     }
@@ -22,6 +27,17 @@ class KbEngineTest: public ::testing::Test {
       return UInt32(charList[charCodeType].at(index) | UNICODE_MASK);
     }
 
-    kbengine engine;
-    map<UInt32, vector<UInt16>> charList;
+    vector<UInt32> sendKeyStrokes(const vector<UInt16> &charCodes) {
+      vector<UInt32> output;
+      for(int i=0; i < charCodes.size(); i++) {
+        char temp = toupper(charCodes[i]);
+        engine.process(charCodes[i], charToKeyCode[temp], 0, false);
+        vector<UInt32> keystrokes = engine.getOutputBuffer();
+
+        for(auto it = keystrokes.begin(); it != keystrokes.end(); it++) {
+          output.push_back(*it);
+        }
+      }
+      return output;
+    }
 };
--- kbengine/src/test/test-main.cc
+++ kbengine/src/test/test-main.cc
@@ -2,23 +2,81 @@
 
 #include "fixture/kbengine-test.cc"
 
-TEST_F(KbEngineTest, TestInput_viet65) {
-  vector<UInt16> keyCodes = { KEY_V, KEY_I, KEY_E, KEY_T, KEY_6, KEY_5 };
-  vector<UInt16> charCodes = { 'v', 'i', 'e', 't' };
+TEST_F(KbEngineTest, TestKeyStroke_Input_viet65) {
+  vector<UInt16> charCodes = { 'v', 'i', 'e', 't', '6', '5' };
 
-  // expect 2 delete keystore + unicode-ê, KEY_T
-  vector<UInt32> expectedOut = { 
-                                 UInt32(KEY_DELETE), UInt32(KEY_DELETE), 
-                                 getVNCharAt(KEY_E | MASK_ROOF, 11), UInt32(KEY_T)
+  // expect 2 delete keystore, unicode-ê, KEY_T
+  vector<UInt32> expectedOut = {
+                                // receive key 6
+                                 UInt32(KEY_DELETE), UInt32(KEY_DELETE), UInt32(KEY_DELETE),      // delete 3 chars
+                                 UInt32(KEY_I), getVNCharAt(KEY_E | MASK_ROOF, 1), UInt32(KEY_T), // add iêt
+                                 // receive key 5
+                                 UInt32(KEY_DELETE), UInt32(KEY_DELETE),                          // delete 2 chars
+                                 getVNCharAt(KEY_E | MASK_ROOF, 11), UInt32(KEY_T),               // add ệt
                                 };
 
   engine.resetBuffer();
   
-  for(int i=0; i < keyCodes.size(); i++) {
-    engine.process(charCodes[i], keyCodes[i], 0, false);
-  }
-
-  vector<UInt32> keystrokes = engine.getOutputBuffer();
+  vector<UInt32> output = sendKeyStrokes(charCodes);
   
-  EXPECT_EQ(keystrokes, expectedOut);
+  EXPECT_EQ(output, expectedOut);
+}
+
+// Test uo7 --> uơ
+TEST_F(KbEngineTest, TestKeyStroke_Input_uo7) {
+  vector<UInt16> charCodes = { 'u', 'o', '7' };
+
+  vector<UInt32> expectedOut = {
+                                // receive key 7
+                                 UInt32(KEY_DELETE),      // delete 1 chars
+                                 getVNCharAt(KEY_O | MASK_HOOK, 1), // add ơ
+                                };
+
+  engine.resetBuffer();
+  
+  vector<UInt32> output = sendKeyStrokes(charCodes);
+  
+  EXPECT_EQ(output, expectedOut);
+}
+
+// Test uo7n --> ươn
+TEST_F(KbEngineTest, TestKeyStroke_Input_uo7n) {
+  vector<UInt16> charCodes = { 'u', 'o', '7', 'n' };
+
+  vector<UInt32> expectedOut = {
+                                  // receive key 7
+                                 UInt32(KEY_DELETE),      // delete 1 chars
+                                 getVNCharAt(KEY_O | MASK_HOOK, 1), // add ơ
+                                 // receive 'n'
+                                 UInt32(KEY_DELETE), UInt32(KEY_DELETE), // delete 2 chars
+                                 getVNCharAt(KEY_U | MASK_HOOK, 1),   // add ư
+                                 getVNCharAt(KEY_O| MASK_HOOK, 1),    // add ơ
+                                 UInt32(KEY_N)                        // add n
+                                };
+
+  engine.resetBuffer();
+  
+  vector<UInt32> output = sendKeyStrokes(charCodes);
+  
+  EXPECT_EQ(output, expectedOut);
+}
+
+
+// Test uon7 --> ươn
+TEST_F(KbEngineTest, TestKeyStroke_Input_uon7) {
+  vector<UInt16> charCodes = { 'u', 'o', 'n', '7' };
+
+  vector<UInt32> expectedOut = {
+                                 // receive 'n'
+                                 UInt32(KEY_DELETE), UInt32(KEY_DELETE), UInt32(KEY_DELETE), // delete 3 chars
+                                 getVNCharAt(KEY_U | MASK_HOOK, 1),   // add ư
+                                 getVNCharAt(KEY_O| MASK_HOOK, 1),    // add ơ
+                                 UInt32(KEY_N)                        // add n
+                                };
+
+  engine.resetBuffer();
+  
+  vector<UInt32> output = sendKeyStrokes(charCodes);
+  
+  EXPECT_EQ(output, expectedOut);
 }
