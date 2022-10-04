48b46cd6bc8ecfc213369356c34db91eb82c9738
Trieu Tri Tam <trieutritam@gmail.com>
2022-10-04 09:03:14 +0700

    Add CMakeLists.txt

--- kbengine/src/kbengine-test.cpp
+++ kbengine/src/kbengine-test.cpp
@@ -1,6 +1,6 @@
- //============================================================================
+//============================================================================
 // Name        : kbengine.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -13,46 +13,39 @@ using namespace std;
 #include "kbengine/kbengine.h"
 #include "kbengine/platforms/macos.h"
 
-int main() {
-	cout << "!!!Hello World 123!!!" << endl; // prints !!!Hello World!!!
-//	char ch = 1;
-	kbengine engine;
+int main()
+{
+		cout << "!!!Hello World 123!!!" << endl; // prints !!!Hello World!!!
+																						//	char ch = 1;
+		kbengine engine;
+		engine.resetBuffer();
 
-	engine.reset();
+		UInt16 seq[] = {KEY_V, KEY_I, KEY_E, KEY_T, KEY_6, KEY_5};
+		UInt16 CharCode[] = {'v', 'i', 'e', 't', '6', '5'};
+		int size = 6;
 
-//	while(true) {
-//		std::cin >> ch;
-//		//cout << ch;
-//
-//		//call to engine for processing
-//		engine.process(ch);
-//
-//		std::cin.clear();
-//
-//		if (ch == '=')
-//			break;
-//	}
-	int seq[] = { KEY_V, KEY_I, KEY_E, KEY_T, KEY_6, KEY_5 };
-	int size = 6;
+		for (int i = 0; i < size; i++)
+		{
+			engine.process(CharCode[i], seq[i], 0, false);
+			vector<UInt32> keystrokes = engine.getOutputBuffer();
 
-	for (int i=0; i < size; i++)
-	{
-		engine.process(seq[i]);
-        
-        vector<UInt16> keystrokes = engine.getOutputBuffer();
-        if (keystrokes.size() == 0) continue;
-        cout << endl << "send keystroke: ";
-        for(auto it = keystrokes.begin(); it != keystrokes.end(); it ++) {
-            cout << (*it) << " ";
-        }
-        cout << endl;
-	}
+			if (keystrokes.size() == 0)
+				continue;
 
-	BufferEntry* buffer = engine.getBuffer();
-	for (int i=0; i<32; i++)
-	{
-		cout << (buffer + i)->keyCode << " ";
-	}
+			cout << endl
+					<< "send keystroke: ";
+			for (auto it = keystrokes.begin(); it != keystrokes.end(); it++)
+			{
+				cout << (*it) << " ";
+			}
+			cout << endl;
+		}
 
-	return 0;
+		BufferEntry *buffer = engine.getBuffer();
+		for (int i = 0; i < 32; i++)
+		{
+				cout << (buffer + i)->keyCode << " ";
+		}
+
+		return 0;
 }
